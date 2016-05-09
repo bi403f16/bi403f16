b3799d4b29c307f9548feed8a8998d196e6b655d
Bjarnaby <bjarnaby@gmail.com>
2016-05-09 10:11:50 +0200

    Shop Class

--- Master/Customer_Master/Model/Shop_class.cs
+++ Master/Customer_Master/Model/Shop_class.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MySql.Data;
-using MySql.Data.MySqlClient;
-
-namespace Classes
+﻿namespace Classes
 {
     public class Shop
     {
