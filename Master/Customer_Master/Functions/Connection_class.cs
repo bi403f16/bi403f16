fe474f9695f4da1350c24db7fb8563f3aa26fe9e
JKoldkur <jeenze.jk@gmail.com>
2016-05-09 10:36:27 +0200

    Slettede Login og Projekt mapperne

---
+++ Master/Customer_Master/Functions/Connection_class.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace MySQLConnection
+{
+    public class Connection
+    {
+        public static void ConnectToDB()
+        {
+            string connStr = "server=localhost;user=root;database=conntest;port=3306;password=¤¤¤¤¤¤;charset=latin1;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                // Tilpas nedenstående query til hvad den skal bruges til!
+                /*
+                    string sql = "SELECT name, age FROM tableinfo";     // Denne kan bruges til insert, delete etc.
+                        MySqlCommand cmd = new MySqlCommand(sql, conn);
+                        MySqlDataReader rdr = cmd.ExecuteReader();
+
+                        while (rdr.Read())
+                      {
+                        Console.WriteLine(rdr[0]+" -- " +rdr[1]);   // [] kan ikke huske om det er array plads?!
+                      }
+                        rdr.Close(); 
+                */
+
+            }
+            catch (Exception ex)       //Fjern MySql fra Exception hvis det giver problemer.
+            {
+
+                // Fjern nedenstående kommentar i Forms!
+
+                /* switch (ex.Number)
+               {
+                   case 0:
+                       MessageBox.Show("Kan ikke skabe forbindelse til serveren. Kontakt administrator.");
+                       break;
+                   case 1045:
+                       MessageBox.Show("Forkert brugernavn eller adgangskode, prøv venligst igen");
+                       break;
+               } */
+
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");     //Skal nok fjernes!
+            Console.ReadLine();             //Skal nok fjernes!
+        }
+
+    }
+}
+
+
+/*
+¤¤¤¤¤
+HUSK AT ADD MySql.Data.dll til refernces
+¤¤¤¤¤
+*/
